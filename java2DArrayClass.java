a9fb5a09dd0247cd3d0acb3e1f82811002a8a04e
Sabarish V S <Sabarish_7@protonmail.com>
2025-04-05 16:31:17 +0530

    finsihed 6th porblem

--- java2DArrayClass.java
+++ java2DArrayClass.java
@@ -6,21 +6,31 @@ public class java2DArrayClass {
          //sets the value of the element at the specified row and column
         public void set(int row , int col, int d){
             if(row <= r && col <= c){
-                 data[row][col] = data;
-            } 
-
+                 data[row][col] = d;
+            }
         }
-        public void get(int row, int col){}
+        public void get(int row, int col){
+            if(row <= r && col <= c){
+              System.out.println(data[row][col]);
+           }
+        }
         public void print(){}
-        TwoDimenstionalArray(int r,int c){
-            this.c = c;
-            this.r = r;
-            data = new int[r][c];
+        TwoDimenstionalArray(int row,int col){
+            if(row <= r && col <= c){
+                for(int i=0; i<row; i++){
+                 for(int j=0; i<col; j++){
+                    System.out.println(data[i][j]);
+                 }   
+                }
+               
+             }
         }
     }
     public static void main(String[] args) {
         TwoDimenstionalArray test = new TwoDimenstionalArray(3, 3);
         test.set(0, 0, 0);
+        test.get(0, 0);
+        test.print();
        
     }
 
